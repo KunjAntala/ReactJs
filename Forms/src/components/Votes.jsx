ce07287e55a8509625aadbef1db4de29498dae15
KunjGit1 <138096211+KunjGit1@users.noreply.github.com>
2024-08-02 15:58:11 +0530

    new

--- Forms/src/components/Votes.jsx
+++ Forms/src/components/Votes.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import './Votes.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { actions } from '../store/votes-store'
 
 function Votes() {
-    // const votes = useSelector(state => state.Votes)
+    const initialState = {votes:0,toggle:true}
+    const votes = useSelector((state = initialState) => state.votes)
+    const showVotes = useSelector((state = initialState) => state.toggle)
     const dispatch = useDispatch()
 
     const incrementHandler = () => {
-        dispatch({type:'Increment'})
+        //   dispatch({type:'Increment'})
+        dispatch(actions.Increment())
     }
 
     const decrementHandler = () => {
-        dispatch({type:'Decrement'})
+        // dispatch({type:'Decrement'})
+        dispatch(actions.Decrement())
     }
 
     const addby10Handler = () => {
-        dispatch({type:'Increse',value:10})
+        // dispatch({type:'Increse',payload:10})
+        dispatch(actions.Increase(10))
     }
 
     const toggleVotesHandler = () => {
-
+        // dispatch({type:'Toggle'})
+        dispatch(actions.Toggle())
     }
     return (
         <div className='votes'>
             <h1>Redux Votes</h1>
-            <div className='value'>Number of votes</div>
+            {showVotes && <div className='value'>{votes}</div>}
             <button onClick={incrementHandler}>Increment</button>
             <button onClick={decrementHandler}>Decrement</button>
             <button onClick={addby10Handler}>Increment</button>
--- Forms/src/store/votes-store.jsx
+++ Forms/src/store/votes-store.jsx
@@ -1,14 +1,28 @@
 import { createStore } from 'redux'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const reducer = (state={votes:0},action) => {
-    if(action.type === 'Increment'){
-        return {votes:state.votes+1}
-    }
-    else if(action.type === 'Decrement'){
-        return {votes:state.votes-1}
-    }
-    else if(action.type === 'Increse'){
-        return {votes:state.votes+action.value}
-    }
+const initialState = {votes:0,toggle:true}
+// const reducerFunction = (state=initialState,action) => {
+//     if(action.type === 'Increment'){
+//         return {votes:state.votes+1,toggle:state.toggle}
+//     }
+//     else if(action.type === 'Decrement'){
+//         return {votes:state.votes-1,toggle:state.toggle}
+//     }
+//     else if(action.type === 'Increse'){
+//         return {votes:state.votes+action.payload,toggle:state.toggle}
+//     }
+//     else if(action.type === 'Toggle'){
+//         return {votes:state.votes,toggle:!state.toggle}
+//     }
+// }
+reducer = {
+    Increment:(state) => state.votes++,
+    Decrement:(state) => state.votes--,
+    Increase:(state,action) => state.votes=state.votes + action.payload,
+    Toggle:(state) => !state.toggle
 }
-export const voteStore = createStore(reducer)
+const voteSlice = createSlice('votes',initialState,reducer)
+export const voteStore = configureStore({reducer:{votes:voteSlice.reducer}})
+export const actions = voteSlice.actions
+// export const voteStore = createStore(voteSlice.reducer)
